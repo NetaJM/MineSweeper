7373b33cb0d07cca1b6850178660ac42875f4524
Neta Milenkovic <92453633+NetaJM@users.noreply.github.com>
2022-03-06 20:09:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,114 +1,106 @@
 
-const MEDIUM = "MEDIUM";
-const EXPERT = "EXPERT";
-const IMPOSSIBLE = "IMPOSSIBLE";
-const DIFFICULTY = {
-  MEDIUM: {
-    mines: 12,
-    dimension: 6 
-  },
-  EXPERT: {
-    mines: 30,
-    dimension: 12
-  },
-  IMPOSSIBLE: {
-    mines: 60,
-    dimension: 12
+let FLAG = '🚩';
+let MINE = '💣';
+
+function getSorroundingSquares(coord) {
+  let sorroundingSquares = [
+    // Top Left
+    { i: coord.i - 1, j: coord.j - 1 },
+    // Top
+    { i: coord.i - 1, j: coord.j },
+    // Top Right
+    { i: coord.i - 1, j: coord.j + 1 },
+    // Left
+    { i: coord.i, j: coord.j - 1 },
+    // Right
+    { i: coord.i, j: coord.j + 1 },
+    // Bottom Left
+    { i: coord.i + 1, j: coord.j - 1 },
+    // Bottom
+    { i: coord.i + 1, j: coord.j },
+    // Bottom Right
+    { i: coord.i + 1, j: coord.j + 1 },
+  ]
+  return sorroundingSquares;
+}
+
+function getSquareElement(coord, forceShow) {
+  let squareInfo = gBoard[coord.i][coord.j];
+  if (squareInfo.isFlagged) {
+    let color = 'red';
+    if (squareInfo.isExploded) {
+      color = 'white'
+    }
+    return '<span style="color:' + color + '">' + FLAG + '</span>';
   }
-}
-let SMILE = '&#128522;';
-let SUNGLASSES = '&#128526;';
-let DEAD = '&#128128;';
-
-let game = {
-  isStarted: false,
-  lives: 3,
-  currentDifficulty: null,
-  time: 0
-}
-let timer;
-let gBoard;
-
-function chooseDifficulty(difficulty) {
-  game.currentDifficulty = difficulty;
-  game.lives = 3;
-  game.time = 0;
-  let elTimer = document.getElementById('timer');
-  elTimer.innerText = 0;
-  let elSmiley = document.getElementById('smiley');
-  elSmiley.innerHTML = '<span>'+SMILE+'</span>';
-  let elLives = document.getElementById('lives');
-  elLives.innerText = 3;
-  let elDifficultyModal = document.getElementById('difficulty');
-  elDifficultyModal.style.display = 'none';
-  let elGameContainer = document.getElementById('game-container');
-  elGameContainer.style.display = 'initial';
-  let elMineInfo = document.getElementById('mines');
-  elMineInfo.innerText = DIFFICULTY[difficulty].mines;
-  buildFakeBoard(game.currentDifficulty);
-  renderBoard();
+  else if ((squareInfo.isShown || forceShow) && squareInfo.isMine) {
+    return '<span>' + MINE + '</span>'
+  }
+  else if (squareInfo.isShown && squareInfo.isNeighbor) {
+    return squareInfo.neighborValue;
+  }
+  return ' ';
 }
 
-function initGame(clickedCoord) {
-  game.isStarted = true;
-  game.lives = 3;
-  buildBoard(clickedCoord);
-  startTimer();
-  clickSquare(clickedCoord);
-}
+function clickSquare(coord) {
+  let squareInfo = gBoard[coord.i][coord.j];
 
-function startTimer() {
-  timer = setInterval(function() {
-    game.time = game.time + 10;
-    let elTimer = document.getElementById('timer');
-    elTimer.innerText = Math.floor(game.time / 1000)
-  }, 10);
-}
+  if (!game.isStarted) {
+    if (game.lives > 0 && !squareInfo.isFlagged) {
+      initGame(coord);
+    }
+    return;
+  }
 
-function stopTimer() {
-  clearInterval(timer);
-}
-
-function checkIfPlayerWon() {
-  let dimension = DIFFICULTY[game.currentDifficulty].dimension;
-  for(let i = 0; i < dimension; i++) {
-    for(let j = 0; j < dimension; j++) {
-      let squareInfo = gBoard[i][j];
-      // If there is at least one unflagged mine, player did not win
-      if (squareInfo.isMine && !squareInfo.isFlagged) { 
-        return false;
-      }
-      // If there is a non-mine square that is still not shown, played did not win
-      if (!squareInfo.isMine && !squareInfo.isShown) {
-        return false;
-      }
+  if (squareInfo.isShown || squareInfo.isFlagged) {
+    return;
+  }
+  else if (squareInfo.isMine) {
+    if (game.lives > 1) {
+      gBoard[coord.i][coord.j].isExploded = true;
+      flagSquare(coord);
+      renderBoard();
+      lifeLost();
+    } else {
+      gBoard[coord.i][coord.j].isExploded = true;
+      gBoard[coord.i][coord.j].isShown = true;
+      renderBoard(true);
+      lifeLost();
+      gameLost();
     }
   }
-  return true;
+  else {
+    gBoard[coord.i][coord.j].isShown = true;
+    revealNeighboringSquares(coord);
+    renderBoard();
+  }
+  let playerWon = checkIfPlayerWon();
+  if (playerWon) {
+    gameWon();
+  }
 }
 
-function gameWon() {
-  game.isStarted = false;
-  
-  stopTimer();
-  let elSmiley = document.getElementById('smiley');
-  elSmiley.innerHTML = '<span>'+SUNGLASSES+'</span>';
-}
-
-function gameLost() {
-  game.isStarted = false;
-  stopTimer();
-  let elSmiley = document.getElementById('smiley');
-  elSmiley.innerHTML = '<span>'+DEAD+'</span>';
-}
-
-function lifeLost() {
-  game.lives--;
-  let elLives = document.getElementById('lives');
-  elLives.innerText = game.lives;
-}
-
-function playAgain() {
-  game.isStarted = false;
-  chooseDifficulty(game.currentDifficulty);
+function flagSquare(coord) {
+  if (!game.isStarted) {
+    return;
+  }
+  if (gBoard[coord.i][coord.j].isShown) {
+    return;
+  }
+  let isFlagged = gBoard[coord.i][coord.j].isFlagged;
+  gBoard[coord.i][coord.j].isFlagged = !isFlagged;
+  let elMineInfo = document.getElementById('mines');
+  let numberLeft = Number(elMineInfo.innerText);
+  if (isFlagged) {
+    numberLeft++;
+  }
+  else {
+    numberLeft--;
+  }
+  elMineInfo.innerText = numberLeft;
+  renderBoard();
+  let playerWon = checkIfPlayerWon();
+  if (playerWon) {
+    gameWon(true);
+  }
 }
