70d880a6b7a197bff16ddf62a612dd9007210987
Neta Milenkovic <92453633+NetaJM@users.noreply.github.com>
2022-03-06 20:08:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 
-const BEGINNER = "BEGINNER";
 const MEDIUM = "MEDIUM";
 const EXPERT = "EXPERT";
 const IMPOSSIBLE = "IMPOSSIBLE";
 const DIFFICULTY = {
-  BEGINNER: {
-    mines: 2,
-    dimension: 4
-  },
   MEDIUM: {
     mines: 12,
     dimension: 6 
@@ -21,34 +16,34 @@ const DIFFICULTY = {
     dimension: 12
   }
 }
-var SMILE = '&#128522;';
-var SUNGLASSES = '&#128526;';
-var DEAD = '&#128128;';
+let SMILE = '&#128522;';
+let SUNGLASSES = '&#128526;';
+let DEAD = '&#128128;';
 
-var game = {
+let game = {
   isStarted: false,
   lives: 3,
   currentDifficulty: null,
   time: 0
 }
-var timer;
-var gBoard;
+let timer;
+let gBoard;
 
 function chooseDifficulty(difficulty) {
   game.currentDifficulty = difficulty;
   game.lives = 3;
   game.time = 0;
-  var elTimer = document.getElementById('timer');
+  let elTimer = document.getElementById('timer');
   elTimer.innerText = 0;
-  var elSmiley = document.getElementById('smiley');
+  let elSmiley = document.getElementById('smiley');
   elSmiley.innerHTML = '<span>'+SMILE+'</span>';
-  var elLives = document.getElementById('lives');
+  let elLives = document.getElementById('lives');
   elLives.innerText = 3;
-  var elDifficultyModal = document.getElementById('difficulty');
+  let elDifficultyModal = document.getElementById('difficulty');
   elDifficultyModal.style.display = 'none';
-  var elGameContainer = document.getElementById('game-container');
+  let elGameContainer = document.getElementById('game-container');
   elGameContainer.style.display = 'initial';
-  var elMineInfo = document.getElementById('mines');
+  let elMineInfo = document.getElementById('mines');
   elMineInfo.innerText = DIFFICULTY[difficulty].mines;
   buildFakeBoard(game.currentDifficulty);
   renderBoard();
@@ -65,7 +60,7 @@ function initGame(clickedCoord) {
 function startTimer() {
   timer = setInterval(function() {
     game.time = game.time + 10;
-    var elTimer = document.getElementById('timer');
+    let elTimer = document.getElementById('timer');
     elTimer.innerText = Math.floor(game.time / 1000)
   }, 10);
 }
@@ -75,10 +70,10 @@ function stopTimer() {
 }
 
 function checkIfPlayerWon() {
-  var dimension = DIFFICULTY[game.currentDifficulty].dimension;
-  for(var i = 0; i < dimension; i++) {
-    for(var j = 0; j < dimension; j++) {
-      var squareInfo = gBoard[i][j];
+  let dimension = DIFFICULTY[game.currentDifficulty].dimension;
+  for(let i = 0; i < dimension; i++) {
+    for(let j = 0; j < dimension; j++) {
+      let squareInfo = gBoard[i][j];
       // If there is at least one unflagged mine, player did not win
       if (squareInfo.isMine && !squareInfo.isFlagged) { 
         return false;
@@ -96,20 +91,20 @@ function gameWon() {
   game.isStarted = false;
   
   stopTimer();
-  var elSmiley = document.getElementById('smiley');
+  let elSmiley = document.getElementById('smiley');
   elSmiley.innerHTML = '<span>'+SUNGLASSES+'</span>';
 }
 
 function gameLost() {
   game.isStarted = false;
   stopTimer();
-  var elSmiley = document.getElementById('smiley');
+  let elSmiley = document.getElementById('smiley');
   elSmiley.innerHTML = '<span>'+DEAD+'</span>';
 }
 
 function lifeLost() {
   game.lives--;
-  var elLives = document.getElementById('lives');
+  let elLives = document.getElementById('lives');
   elLives.innerText = game.lives;
 }
 
